ffc3ec1be1fa1a4d8dd371c9ee56d370db27d1f8
Alehmann04 <xXKamuiKaziXx@gmail.com>
2025-04-01 11:32:51 -0400

    Added comments explaining each method

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import time
 import asyncio
 import logging
 import sqlite3
@@ -13,10 +12,11 @@ from twitchio import eventsub
 
 LOGGER: logging.Logger = logging.getLogger("Bot")
 
+
 class Bot(commands.Bot):
     def __init__(self, *, token_database: asqlite.Pool) -> None:
         self.token_database = token_database
-        
+
         self.owner_name=os.environ['OWNER_NAME']
         self.bot_name=os.environ['BOT_NAME']
         self.target_id=os.environ['TARGET_ID']
@@ -29,7 +29,7 @@ class Bot(commands.Bot):
             owner_id=os.environ['OWNER_ID'],
             prefix=os.environ['BOT_PREFIX'],
         )
-    
+
     async def event_ready(self):
         # When the bot is ready
         LOGGER.info("Successfully logged in as: %s", self.bot_id)
@@ -37,6 +37,7 @@ class Bot(commands.Bot):
         # await target.send_message(sender=self.bot_id, message='Bot has landed')
 
     #oauth token portion
+    #   setting up the webhooks and adding compontent object
     async def setup_hook(self) -> None:
         # Add our component which contains our commands...
         await self.add_component(MyComponent(self))
@@ -45,7 +46,8 @@ class Bot(commands.Bot):
         # For this example listen to our target's stream...
         subscription = eventsub.ChannelRaidSubscription(to_broadcaster_user_id=self.target_id)
         await self.subscribe_websocket(payload=subscription)
-
+    
+    #   adds tokens from localhost:433
     async def add_token(self, token: str, refresh: str) -> twitchio.authentication.ValidateTokenPayload:
         # Make sure to call super() as it will add the tokens interally and return us some data...
         resp: twitchio.authentication.ValidateTokenPayload = await super().add_token(token, refresh)
@@ -66,6 +68,7 @@ class Bot(commands.Bot):
         LOGGER.info("Added token to the database for user: %s", resp.user_id)
         return resp
 
+    #   loads tokens from database
     async def load_tokens(self, path: str | None = None) -> None:
         # We don't need to call this manually, it is called in .login() from .start() internally...
 
@@ -75,30 +78,30 @@ class Bot(commands.Bot):
         for row in rows:
             await self.add_token(row["token"], row["refresh"])
 
+    #   sets the structure of the token database
     async def setup_database(self) -> None:
         # Create our token table, if it doesn't exist..
         query = """CREATE TABLE IF NOT EXISTS tokens(user_id TEXT PRIMARY KEY, token TEXT NOT NULL, refresh TEXT NOT NULL)"""
         async with self.token_database.acquire() as connection:
             await connection.execute(query)
-    
 
+    
 class MyComponent(commands.Component):
     def __init__(self, bot: Bot):
         self.bot = bot
-
+        
     @commands.Component.listener()
     async def event_raid(self, payload: twitchio.ChannelRaid) -> None:
         # Event dispatched when a user gets a raid from the subscription we made above...
-        if (payload.from_broadcaster.id != self.bot.target_id) and (payload.to_broadcaster.id == self.bot.target_id):
-            await payload.to_broadcaster.send_shoutout(
-                to_broadcaster=payload.from_broadcaster.id,
-                moderator=self.bot.bot_id,
-            )
-            time.sleep(1)
-            await payload.to_broadcaster.send_message(
-                sender=self.bot.bot_id,
-                message="!raiders",
-            )
+        await payload.to_broadcaster.send_shoutout(
+            to_broadcaster=payload.from_broadcaster.id,
+            moderator=self.bot.bot_id,
+        )
+        # await asyncio.sleep(1)
+        # await payload.to_broadcaster.send_message(
+        #     sender=self.bot_id,
+        #     message="!raiders",
+        # )
         LOGGER.info(f"[Raid detected] - {payload.from_broadcaster.display_name} is raiding {payload.to_broadcaster.display_name} with {payload.viewer_count} viewers!")
 
 def main() -> None:
